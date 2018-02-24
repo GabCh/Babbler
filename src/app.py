992742ff128fa84e897f98b2cfbccd8cc352f47e
gabriel chantal <gabriel.chantal.1@ulaval.ca>
2018-02-24 16:42:32 -0500

    Adding first version of search

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
     return "Bonjour et bienvenu!"
+
+
+@app.route("/search")
+def search_form():
+    keyword = request.args.get('keyword')
+    if keyword:
+        return render_template('search_results.html', keyword=keyword)
+    else:
+        return render_template('search.html')
 
 
 if __name__ == "__main__":
