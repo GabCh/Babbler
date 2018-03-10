077ff11e9d5b3420cdfaa5b3d65c351ecca9d3f2
gabriel chantal <gabriel.chantal.1@ulaval.ca>
2018-03-10 15:20:15 -0500

    REMOVING CANCER SQL CONNECTION

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
-import pymysql.cursors
-from time import gmtime, strftime
-import json
 import os
+
 from flask import Flask, render_template, request, session, url_for, redirect
 
 
@@ -23,31 +21,6 @@ users = [
         'password': ''
     }
 ]
-
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='minecraft371',
-                             db='Babbler',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-with connection.cursor() as cursor:
-    # Create a new record
-    sql = "INSERT INTO `babbles` (`id`, `pseudo`, `message`, `time_s`, `tags`) VALUES (%s, %s, %s, %s, %s)"
-    cursor.execute(sql, (12345, 'blazeboy420', 'Test de babble',
-                         strftime("%Y-%m-%d %H:%M:%S", gmtime()), json.dumps({"keyword": ["banane", "blaze"]})))
-
-# connection is not autocommit by default. So you must commit to save
-# your changes.
-connection.commit()
-
-with connection.cursor() as cursor:
-    # Read a single record
-    sql = "SELECT * FROM `babbles`"
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    print(result)
 
 
 @app.route('/')
