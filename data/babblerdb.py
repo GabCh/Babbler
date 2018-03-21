618ac9b0748a0b34553112f5d992674e78f98936
jannik levesque <jannik.levesque.1@ulaval.ca>
2018-03-21 15:05:12 -0400

    adding insert methods

--- data/babblerdb.py
+++ data/babblerdb.py
@@ -14,28 +14,38 @@ class BabblerDB(object):
         query = "".join(sql)
         self.connection.cursor().execute(query)
 
-    def create_table_babblers(self):
+    def add_babbler(self, username, publicName, password):
         sql = """
-            CREATE TABLE babblers (
-            Username VARCHAR(16) PRIMARY KEY,
-            PublicName VARCHAR(16),
-            Password VARCHAR(32)
-            );
-        """
-        self.cursor.execute(sql)
-        print('Created babblers table!')
-
-    def add_babbler(self, username, public_name, password):
-        sql = """
-            INSERT INTO babblers (Username, PublicName, Password)
-            VALUES (""" + username + ',' + public_name + ',' + password + ');'
-        self.cursor.execute(sql)
+            INSERT INTO Babblers (username, publicName, password)
+            VALUES ( %s , %s , %s);"""
+        #try:
+            #self.cursor.execute(sql, (username, publicName, password))
         print('Inserted ' + username + ' into table babblers!')
+
+    def add_babble(self, id, username, message, time_s, tags):
+        sql = """
+            INSERT INTO Babbles (id, username, message, time_s)
+            VALUES (%s, %s, %s, %s);"""
+        self.cursor.execute(sql, (id, username, message, time_s))
+        self.add_tags(id, tags)
+
+    def add_tags(self, id, tags):
+        for t in tags:
+            sql = """
+                INSERT INTO Tag (id, tag)
+                VALUES (%s, %s);"""
+            self.cursor.execute(sql, (id, t))
+
+    def add_follower(self, follower, followed):
+        sql = """
+            INSERT INTO Follows (follower, followed)
+            VALUES (%s, %s);"""
+        self.cursor.execute(sql, (follower, followed))
 
     def read_table(self, table, keyword, attribute):
         sql = list()
-        sql.append("SELECT * FROM %s " % table)
-        sql.append("WHERE %s LIKE '%%%s%%'" % (attribute, keyword))
+        sql.append("SELECT * FROM %s ", (table,))
+        sql.append("WHERE %s LIKE '%%%s%%'", (attribute, keyword))
         sql.append(";")
         query = "".join(sql)
         cur = self.connection.cursor()
