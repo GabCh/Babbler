0c03560606e08d468c1f5292f8c496a399ae7100
gabriel chantal <gabriel.chantal.1@ulaval.ca>
2018-03-21 17:19:22 -0400

    Added utils for ellapsed time

---
+++ data/utils.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timedelta
+
+YEARS = 31557600
+MONTHS = 2629800
+WEEKS = 604800
+DAYS = 86400
+HOURS = 3600
+MINUTES = 60
+
+
+def get_elapsed_time(date_str: str):
+    present = datetime.now()
+    post_date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+    delta = present - post_date
+    seconds = delta.total_seconds()
+    ellapsed = ''
+
+    years = int(seconds / YEARS)
+    seconds -= (years * YEARS)
+    if years:
+        if years > 1:
+            ellapsed += str(years) + 'yrs '
+        else:
+            ellapsed += str(years) + 'yr '
+
+    months = int(seconds / MONTHS)
+    seconds -= (months * MONTHS)
+    if months:
+        if months > 1:
+            ellapsed += str(months) + ' months '
+        else:
+            ellapsed += str(months) + ' month '
+        return ellapsed
+
+    weeks = int(seconds / WEEKS)
+    seconds -= (weeks * WEEKS)
+    if weeks:
+        ellapsed += str(weeks) + 'w '
+        return ellapsed
+
+    days = int(seconds / DAYS)
+    seconds -= (days * DAYS)
+    if days:
+        ellapsed += str(days) + 'd '
+        return ellapsed
+
+    hours = int(seconds / HOURS)
+    seconds -= (hours * HOURS)
+    if hours:
+        ellapsed += str(hours) + 'h '
+
+    minutes = int(seconds / MINUTES)
+    seconds -= int(minutes * MINUTES)
+    if minutes:
+        ellapsed += str(minutes) + 'm '
+
+    return ellapsed
